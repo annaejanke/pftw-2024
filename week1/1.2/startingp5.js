59256e73bc16fc0676375eb55ba2fb47b3b347e7
annaejanke <150176824+annaejanke@users.noreply.github.com>
2024-01-18 18:38:46 -0600

    1.2

---
+++ week1/1.2/startingp5.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas (2000, 1000);
+
+}
+
+function draw() {
+    if (mouseIsPressed) {
+        fill(226, 209, 249);
+        ellipse(mouseX, mouseY, 80, 80);
+      } else {
+        
+      }
+}
