48d307e86be77514d4ee606a5d07ba6d3e3f0657
annaejanke <150176824+annaejanke@users.noreply.github.com>
2024-01-25 21:28:17 -0600

    fix string output formatting issues

--- week2/2.1/arrays.js
+++ week2/2.1/arrays.js
@@ -1,13 +1,13 @@
-const movies = ['Harry Potter', 'Something Borrowed', 'Guardians of the Galaxy', 'Thor', 'The Devil Wears Prada'];
+const movies = [' Harry Potter', ' Something Borrowed', ' Guardians of the Galaxy', ' Thor', ' The Devil Wears Prada'];
 
 // Prompt the user for input
 const userMovieInput = window.prompt('What is your favorite movie?');
 
 // Push user input to movie array
-movies.push(userMovieInput);
+movies.push(' ' + userMovieInput);
 
 // Create output string for both outputs
-const outputString = "Favorite Movie's" + movies.toString();
+const outputString = "Favorite Movie's: " + movies.toString();
 
 // Outputs
 console.log(outputString);
