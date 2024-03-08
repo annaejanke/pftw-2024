a668afb1cf240be578cbe44a7907685b2c97a429
annaejanke <150176824+annaejanke@users.noreply.github.com>
2024-03-07 20:38:19 -0600

    8.1

--- week8/8.1/src/App.jsx
+++ week8/8.1/src/App.jsx
@@ -2,32 +2,21 @@ import "./App.css"
 import {useState} from "react";
 
 function App () {
-  const [scoreObj, setScoreObj] = useState({left: 0, right:0})
-  function incrementRightScore () {
-    const newScore = {
-      left: scoreObj.left,
-      right: scoreObj.right + 1
-    }
-    setScoreObj(newScore);
-  }
+  const [image, setImage] = useState("#");
+  
 
   return (
     <>
-      <h1>Solitare Clicky Game</h1>
+      <h1>Click on the button to view the image</h1>
       <div>
-        left
-        <button onClick={() => {
-          setScoreObj({...scoreObj, left: scoreObj.left + 1});
-        }}>Push Me</button>
+        <img src={image}></img>
       </div>
-      <div>
-        right
-        <button onClick={incrementRightScore}>No Push Me</button>
-      </div>
-      <div>
-        <h2>Scores</h2>
-        Left Score {scoreObj.left} | Right Score {scoreObj.right}
-      </div>
+      <button onClick={() => {
+        setImage("./alpaca.jpg")
+      }}>Alpaca</button>
+      <button onClick={() => {
+        setImage("./llama.jpg")
+      }}>Llama</button>
     </>
   )
 }
