091c43d715e44dd22a2d954ac5c6029436ab604a
annaejanke <150176824+annaejanke@users.noreply.github.com>
2024-03-25 18:42:08 -0500

    9.1 updates

--- week9/9.1/src/App.jsx
+++ week9/9.1/src/App.jsx
@@ -3,45 +3,110 @@ import { useState } from "react"
 import "./App.css"
 
 function App() {
-  const [textInputs, setTextInputs] = useState({
+  const [data, setData] = useState({
     firstName: "",
     lastName: "",
-    streetAdress: "",
+    streetAddress: "",
     state: "",
     country: "",
+    colors: [],
   })
+
+  function handleTextChange(evt) {
+    setData((previousData) =>{
+      if(evt.target)
+      return {
+        ...previousData,
+        [evt.target.name]: evt.target.value
+      }
+    })
+  }
+
+  function handleColorChange(evt) {
+    let newColors;
+
+    if (data.colors.includes(evt.target.id)) {
+      newColors = data.colors.filter((color) => color !== evt.target.id)
+    } else {
+      newColors = [...data.colors, evt.target.id]
+    }
+
+    setData((previousData) => {
+      return {
+        ...previousData,
+        colors: newColors,
+      }
+    })
+
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    console.log(data);
+  }
+
 
   return (
     <>
     <h1>Alpaca Fan Club Registration Form</h1>
-    <form action="#">
+    <form onSubmit={handleSubmit}>
 
       <fieldset>
         <legend>Personal Information</legend>
 
         <div>
-          <label for="name">First Name: </label>
-          <input type="text" name="pinfo" id="firstName" />
+          <label htmlFor="firstName">First Name: </label>
+          <input 
+            type="text" 
+            name="firstName" 
+            id="firstName" 
+            value={data.firstName} 
+            onChange={handleTextChange}
+          />
         </div>
 
         <div>
-          <label for="pwd">Last Name: </label>
-          <input type="text" name="pinfo" id="lastName" />
+          <label htmlFor="lastName">Last Name: </label>
+          <input 
+            type="text" 
+            name="lastName" 
+            id="lastName" 
+            value={data.lastName} 
+            onChange={handleTextChange}
+          />
         </div>
 
         <div>
-          <label for="pwd">Street Address: </label>
-          <input type="text" name="pinfo" id="streetAddress" />
+          <label htmlFor="streetAddress">Street Address: </label>
+          <input 
+            type="text" 
+            name="streetAddress" 
+            id="streetAddress" 
+            value={data.streetAddress} 
+            onChange={handleTextChange}
+          />
         </div>
 
         <div>
-          <label for="pwd">State: </label>
-          <input type="text" name="pinfo" id="state" />
+          <label htmlFor="state">State: </label>
+          <input 
+            type="text" 
+            name="state" 
+            id="state" 
+            value={data.state}
+            onChange={handleTextChange}
+          />
         </div>
 
         <div>
-          <label for="pwd">Country: </label>
-          <input type="text" name="pinfo" id="country" />
+          <label htmlFor="country">Country: </label>
+          <input 
+            type="text" 
+            name="country" 
+            id="country" 
+            value={data.country}
+            onChange={handleTextChange} 
+          />
         </div>
 
       </fieldset>
@@ -50,24 +115,59 @@ function App() {
         <legend>Alpaca Information</legend>
         <p>Favorite Alpaca Colors:</p>
 
-            <input type="radio" id="purple" name="fav_color"/>
-            <label for="html">Purple</label>
+            <input 
+              type="checkbox" 
+              id="purple" 
+              name="favColor"
+              checked={data.colors.includes("purple")}
+              onChange={handleColorChange}
+            />
+            <label htmlFor="html">Purple</label>
 
-            <input type="radio" id="pink" name="fav_color"/>
-            <label for="css">Pink</label>
+            <input 
+              type="checkbox" 
+              id="pink" 
+              name="favColor"
+              checked={data.colors.includes("pink")}
+              onChange={handleColorChange}
+            />
+            <label htmlFor="css">Pink</label>
 
-            <input type="radio" id="orange" name="fav_color"/>
-            <label for="javascript">Orange</label>
+            <input 
+              type="checkbox" 
+              id="orange" 
+              name="favColor"
+              checked={data.colors.includes("orange")}
+              onChange={handleColorChange}
+            />
+            <label htmlFor="javascript">Orange</label>
 
-            <input type="radio" id="yellow" name="fav_color"/>
-            <label for="javascript">Yellow</label>
+            <input 
+              type="checkbox" 
+              id="yellow" 
+              name="favColor"
+              checked={data.colors.includes("yellow")}
+              onChange={handleColorChange}
+            />
+            <label htmlFor="javascript">Yellow</label>
 
       </fieldset>
 
-      <input type="submit" name="subbutton" id="submit" value="Submit"></input>
-      <input type="reset" name="subbutton" id="reset" value="Reset"></input>
+      <button type="submit" name="subbutton" id="submit" value="Submit">Submit</button>
     
     </form>
+
+    <div>
+      <p>First Name: {data.firstName}</p>
+      <p>Last Name: {data.lastName}</p>
+      <p>Street Address: {data.streetAddress}</p>
+      <p>State: {data.state}</p>
+      <p>Country: {data.country}</p>
+      <p>Colors:</p>
+      {data.colors.map((color) => (
+        <p key={color}>{color}</p>
+      ))}
+    </div>
     </>
   )
 }
